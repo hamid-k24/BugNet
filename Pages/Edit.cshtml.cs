6a32c7131b1e8e512dab1d115b820fec824d5675
Karimi <Karimi@DESKTOP-NRJRRBE>
2023-08-08 04:48:42 +0330

    Edit

---
+++ Pages/Edit.cshtml.cs
@@ -0,0 +1,33 @@
+using BugNet.Data;
+using BugNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugNet.Pages;
+
+public class EditModel : PageModel
+{
+    private readonly BugDbContext _db;
+
+    public EditModel(BugDbContext db)
+    {
+        _db = db;
+    }
+    [BindProperty]
+    public Bug? SelectedBug { get; set; }
+
+    public void OnGet(int id)
+    {
+        SelectedBug = _db.Bugs.Find(id);
+    }
+    public IActionResult OnPost()
+    {
+        var bug= _db.Bugs.Find(SelectedBug.Id);
+        bug.Name = SelectedBug.Name;
+        bug.Description = SelectedBug.Description;
+        bug.IsDone=SelectedBug.IsDone;
+
+        _db.SaveChanges();
+        return RedirectToPage("./index");
+    }
+}
